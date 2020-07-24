cfe658e5df908ee662dac29854176d6b83d38f02
Sambhav <sambhav@MacBook-Pro-6.home>
2020-07-24 17:17:39 -0400

    props and states

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import { Menu } from "./components/MenuComponent";
 import "./App.css";
+import { DISHES } from "./shared/dishes";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu></Menu>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}></Menu>
+      </div>
+    );
+  }
 }
 
 export default App;
